06393fa0a8aef6856ef183293923ea92f3c8fec0
iloveooz <iloveooz@yandex.ru>
2019-01-11 17:56:51 +0700

    a program can find contours in the image

--- main.cpp
+++ main.cpp
@@ -13,10 +13,24 @@ int main() {
     cv::cvtColor(img0, img1, CV_RGB2GRAY);
 
     // применяем фильтр canny
-    
+    cv::Canny(img1, img1, 100, 200);
+
+    // находим контуры
+    std::vector <std::vector<cv::Point> > contours;
+    cv::findContours(img1, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+    // создаём маску
+    cv::Mat mask = cv::Mat::zeros(img1.rows, img1.cols, CV_8UC1);
+
+    // CV_FILLED заполняет найденные компоненты
+    cv::drawContours(mask, contours, -1, cv::Scalar(255), CV_FILLED);
+
+    // создаём ещё одно изображение
+
 
     // показываем изображение
     cv::imshow("original", img0);
+    cv::imshow("mask", mask);
     cv::imshow("canny", img1);
 
     // ожидание нажатия клавиши
@@ -24,5 +38,6 @@ int main() {
 
     // сохраняем изображения
     cv::imwrite("../images/apple_canny.jpg", img1);
+    cv::imwrite("../images/mask.jpg", mask);
     return 0;
 }
