28212ba0c5df92274425d55dbb31b2a50fb67a5e
iloveooz <iloveooz@yandex.ru>
2019-01-11 13:57:09 +0700

    created an empty project

---
+++ cmake-build-debug/CMakeFiles/opencv_findcontours.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_findcontours.dir/main.cpp.o"
+  "opencv_findcontours.pdb"
+  "opencv_findcontours"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_findcontours.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
