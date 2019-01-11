6dcd5ddd82377bdbdde9900267feb4722c0a18b5
iloveooz <iloveooz@yandex.ru>
2019-01-11 14:40:25 +0700

    a program can modify image from RGB to GRAY and save received image

--- main.cpp
+++ main.cpp
@@ -7,10 +7,22 @@ int main() {
     // читаем изображение
     cv::Mat img0 = cv::imread("../images/apple.jpg", 1);
 
+    cv::Mat img1;
+
+    // преобразуем RGB изображение в оттенки серого
+    cv::cvtColor(img0, img1, CV_RGB2GRAY);
+
+    // применяем фильтр canny
+    
+
     // показываем изображение
     cv::imshow("original", img0);
+    cv::imshow("canny", img1);
 
+    // ожидание нажатия клавиши
     cv::waitKey(0);
 
+    // сохраняем изображения
+    cv::imwrite("../images/apple_canny.jpg", img1);
     return 0;
 }
